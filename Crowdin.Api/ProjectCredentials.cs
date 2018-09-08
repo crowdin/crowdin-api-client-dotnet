d49e56aaaa5d42046394b64738052fcebc7bc8ed
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-08 22:48:37 +0300

    Add builder of Crowdin request message.

---
+++ Crowdin.Api/ProjectCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    public sealed class ProjectCredentials
+    {
+        public String ProjectId { get; set; }
+
+        public String ProjectKey { get; set; }
+    }
+}
