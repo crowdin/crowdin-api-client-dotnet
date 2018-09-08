935fb6ee11eb13db2fbf0958259fce174c818d5f
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-08 22:49:19 +0300

    Add dependency on HttpClient to the Client class.

--- Crowdin.Api/Client.cs
+++ Crowdin.Api/Client.cs
@@ -1,8 +1,44 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Crowdin.Api
 {
-    public class Client
+    public sealed partial class Client : IDisposable
     {
+        public Client(Uri baseAddress) : this(new HttpClient {BaseAddress = baseAddress})
+        { }
+
+        public Client(HttpClient httpClient)
+        {
+            HttpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private HttpClient HttpClient { get; }
+
+        private Task<HttpResponseMessage> SendApiRequest(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return HttpClient.SendAsync(request, cancellationToken);
+        }
+
+        ~Client()
+        {
+            Dispose(false);
+        }
+
+        private void Dispose(Boolean disposing)
+        {
+            if (disposing)
+            {
+                HttpClient.Dispose();
+            }
+        }
     }
 }
