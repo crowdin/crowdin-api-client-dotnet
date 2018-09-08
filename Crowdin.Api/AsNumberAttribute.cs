38e3d7900570ca16f980bc6cdc4a952f7c5255b5
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-08 22:47:51 +0300

    Add serializer of objects to format of Crowdin request.

---
+++ Crowdin.Api/AsNumberAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    [AttributeUsage(AttributeTargets.Enum)]
+    internal sealed class AsNumberAttribute : Attribute
+    { }
+}
---
+++ Crowdin.Api/RequiredAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    internal sealed class RequiredAttribute : Attribute
+    { }
+}
