af007530af45b45d356c4bb8aaec3d016eed5c3b
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-10 18:49:54 +0300

    Add folders related API methods.

---
+++ Crowdin.Api/DeleteFolderParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    public sealed class DeleteFolderParameters
+    {
+        [Required]
+        public String Name { get; set; }
+
+        public String Branch { get; set; }
+    }
+}
