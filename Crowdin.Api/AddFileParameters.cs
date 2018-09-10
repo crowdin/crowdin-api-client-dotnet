98e0cb4fa3e681d929776a9f733dca40b41a99fb
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-10 18:47:17 +0300

    Add file related API methods.

---
+++ Crowdin.Api/AddFileParameters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Crowdin.Api
+{
+    public sealed class AddFileParameters
+    {
+        [Required]
+        public IDictionary<String, FileInfo> Files { get; set; }
+
+        public IDictionary<String, String> Titles { get; set; }
+
+        [Property("export_patterns")]
+        public IDictionary<String, String> ExportPatterns { get; set; }
+
+        public String Type { get; set; }
+
+        [Property("first_line_contains_header")]
+        public Boolean? FirstLineContainsHeader { get; set; }
+
+        [Property("import_translations")]
+        public Boolean? ImportTranslations { get; set; }
+
+        public String Scheme { get; set; }
+
+        public String Branch { get; set; }
+
+        [Property("translate_content")]
+        public Boolean? TranslateContent { get; set; }
+
+        [Property("translate_attributes")]
+        public Boolean? TranslateAttributes { get; set; }
+
+        [Property("translatable_elements")]
+        public IEnumerable<String> TranslatableElements { get; set; }
+
+        [Property("escape_quotes")]
+        public EscapeQuotesOption EscapeQuotes { get; set; }
+    }
+}
---
+++ Crowdin.Api/ExportFileParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    public sealed class ExportFileParameters
+    {
+        [Required]
+        public String File { get; set; }
+
+        [Required]
+        public String Language { get; set; }
+
+        public String Branch { get; set; }
+
+        public String Format { get; set; }
+
+        [Property("export_translated_only")]
+        public Boolean? ExportTranslatedOnly { get; set; }
+
+        [Property("export_approved_only")]
+        public Boolean? ExportApprovedOnly { get; set; }
+    }
+}
