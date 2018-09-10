6fcd7cc8006be33fc1b744a147576db16867fe92
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-10 18:52:20 +0300

    Add glossary and translation-memory related API methods.

---
+++ Crowdin.Api/DownloadGlossaryParameters.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    public sealed class DownloadGlossaryParameters
+    {
+        [Property("include_assigned")]
+        public Boolean? IncludeAssigned { get; set; }
+    }
+}
---
+++ Crowdin.Api/DownloadTranslationMemoryParameters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Crowdin.Api
+{
+    public class DownloadTranslationMemoryParameters
+    {
+        [Property("include_assigned")]
+        public Boolean? IncludeAssigned { get; set; }
+
+        [Property("source_language")]
+        public String SourceLanguage { get; set; }
+
+        [Property("target_language")]
+        public String TargetLanguage { get; set; }
+    }
+}
