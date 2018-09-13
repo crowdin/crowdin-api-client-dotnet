4bfd54737a3675c9c560c14198b338d308505df2
Yuriy Kushnir <yuriy.kushnir@crowdin.com>
2018-09-13 10:43:24 +0300

    Add client methods to read strongly typed responses.

--- src/Crowdin.Api/Client.cs
+++ src/Crowdin.Api/Client.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Crowdin.Api
 {
@@ -14,7 +17,29 @@ namespace Crowdin.Api
 
         private HttpClient HttpClient { get; }
 
-        public Task<HttpResponseMessage> SendApiRequest(String url, AccountCredentials credentials, Object body, CancellationToken cancellationToken)
+        public async Task<T> SendApiRequest<T>(String url, AccountCredentials credentials,
+            Object body = null,
+            String payloadProperty = null,
+            CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await SendApiRequest(url, credentials, body, cancellationToken);
+            T result = await ReadApiResponse<T>(response, payloadProperty, cancellationToken);
+            return result;
+        }
+
+        public async Task<T> SendApiRequest<T>(String url, ProjectCredentials credentials,
+            Object body = null,
+            String payloadProperty = null,
+            CancellationToken cancellationToken = default)
+        {
+            HttpResponseMessage response = await SendApiRequest(url, credentials, body, cancellationToken);
+            T result = await ReadApiResponse<T>(response, payloadProperty, cancellationToken);
+            return result;
+        }
+
+        public Task<HttpResponseMessage> SendApiRequest(String url, AccountCredentials credentials,
+            Object body = null,
+            CancellationToken cancellationToken = default)
         {
             HttpRequestMessage request = new HttpRequestMessageBuilder()
                 .SetUri(url)
@@ -24,7 +49,9 @@ namespace Crowdin.Api
             return SendApiRequest(request, cancellationToken);
         }
 
-        public Task<HttpResponseMessage> SendApiRequest(String url, ProjectCredentials credentials, Object body, CancellationToken cancellationToken)
+        public Task<HttpResponseMessage> SendApiRequest(String url, ProjectCredentials credentials,
+            Object body = null,
+            CancellationToken cancellationToken = default)
         {
             HttpRequestMessage request = new HttpRequestMessageBuilder()
                 .SetUri(url)
@@ -37,6 +64,66 @@ namespace Crowdin.Api
         private Task<HttpResponseMessage> SendApiRequest(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             return HttpClient.SendAsync(request, cancellationToken);
+        }
+
+        private async Task<T> ReadApiResponse<T>(HttpResponseMessage response, String payloadProperty, CancellationToken cancellationToken)
+        {
+            if (response.Content.Headers.ContentType.MediaType != "application/json")
+            {
+                throw new InvalidOperationException("Only JSON content is acceptable.");
+            }
+            String json = await response.Content.ReadAsStringAsync();
+            JToken jToken = await ParseJsonAsync(json, cancellationToken);
+            JToken payloadToken = GetPayloadToken(jToken, payloadProperty);
+
+            return payloadToken.ToObject<T>();
+        }
+
+        private async Task<JToken> ParseJsonAsync(String json, CancellationToken cancellationToken)
+        {
+            using (var textReader = new StringReader(json))
+            {
+                using (var jsonReader = new JsonTextReader(textReader))
+                {
+                    JToken jToken = await JToken.LoadAsync(jsonReader, cancellationToken);
+                    return jToken;
+                }
+            }
+        }
+
+        private JToken GetPayloadToken(JToken response, String payloadProperty)
+        {
+            EnsureSuccess(response);
+            if (String.IsNullOrEmpty(payloadProperty))
+            {
+                return response;
+            }
+
+            if (response is JObject jObject && jObject.ContainsKey(payloadProperty))
+            {
+                return jObject[payloadProperty];
+            }
+
+            throw new ArgumentException($"Response object does not contain '{payloadProperty}' property.", nameof(response));
+        }
+
+        private void EnsureSuccess(JToken response)
+        {
+            if (!(response is JObject jObject))
+            {
+                return;
+            }
+
+            JProperty successProperty = jObject.Property("success");
+            if (successProperty?.Value is JValue jValue && jValue.Type == JTokenType.Boolean && !(Boolean)jValue.Value)
+            {
+                JProperty errorProperty = jObject.Property("error");
+                if (errorProperty?.Value is JObject errorObject)
+                {
+                    Error error = errorObject.ToObject<Error>();
+                    throw new CrowdinException(error.Message, error.Code);
+                }
+            }
         }
     }
 }
