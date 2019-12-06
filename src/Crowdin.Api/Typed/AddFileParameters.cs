600f50edee2a34f1ec7d9fd4e0e283508ab3b397
Hasso <hasso_pape@sil.org>
2019-12-06 10:33:29 -0600

    Abstract File Parameters
    Make AddFileParameters and UpdateFileParameters inherit from a new, abstract class

--- src/Crowdin.Api/Typed/AddFileParameters.cs
+++ src/Crowdin.Api/Typed/AddFileParameters.cs
@@ -1,31 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using Crowdin.Api.Protocol;
 
 namespace Crowdin.Api.Typed
 {
-    public sealed class AddFileParameters
+    public sealed class AddFileParameters : FileParameters
     {
-        [Required]
-        public IDictionary<String, FileInfo> Files { get; set; }
-
-        public IDictionary<String, String> Titles { get; set; }
-
-        [Property("export_patterns")]
-        public IDictionary<String, String> ExportPatterns { get; set; }
-
-        public String Type { get; set; }
-
-        [Property("first_line_contains_header")]
-        public Boolean? FirstLineContainsHeader { get; set; }
-
         [Property("import_translations")]
         public Boolean? ImportTranslations { get; set; }
-
-        public String Scheme { get; set; }
-
-        public String Branch { get; set; }
 
         [Property("translate_content")]
         public Boolean? TranslateContent { get; set; }
