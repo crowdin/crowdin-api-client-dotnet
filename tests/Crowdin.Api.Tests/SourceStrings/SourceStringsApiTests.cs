a2c749b2c33b651806f3d9c81af30ce4263209f9
Max Inno <maksym.hubach@gmail.com>
2022-01-04 20:35:34 +0200

    Source Strings: fix scope query param pass

---
+++ tests/Crowdin.Api.Tests/SourceStrings/SourceStringsApiTests.cs
@@ -0,0 +1,23 @@
+﻿
+using Crowdin.Api.SourceStrings;
+using Crowdin.Api.Tests.Core;
+using Xunit;
+
+namespace Crowdin.Api.Tests.SourceStrings
+{
+    public class SourceStringsApiTests
+    {
+        [Fact]
+        public void ListStrings_QueryStringConstruction()
+        {
+            const string expectedQueryString = "limit=25&offset=0&scope=context";
+
+            var @params = new StringsListParams
+            {
+                Scope = StringScope.Context
+            };
+            
+            Assert.Equal(expectedQueryString, @params.ToQueryParams().ToQueryString());
+        }
+    }
+}
