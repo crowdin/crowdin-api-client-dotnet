fb3ebc9baab0b589913d4cbe8e63ca061e0885b6
Max Inno <maksym.hubach@gmail.com>
2022-09-07 20:44:56 +0300

    API Updates: add VendorManualTaskCreateForm

---
+++ src/Crowdin.Api/Tasks/TaskVendor.cs
@@ -0,0 +1,53 @@
+﻿
+using System.ComponentModel;
+using JetBrains.Annotations;
+
+namespace Crowdin.Api.Tasks
+{
+    [PublicAPI]
+    public enum TaskVendor
+    {
+        // ReSharper disable IdentifierTypo
+        
+        [Description("alconost")]
+        Alconost,
+        
+        [Description("babbleon")]
+        BabbleOn,
+        
+        [Description("tomedes")]
+        Tomedes,
+        
+        [Description("e2f")]
+        E2F,
+        
+        [Description("write_path_admin")]
+        WritePath,
+        
+        [Description("inlingo")]
+        Inlingo,
+        
+        [Description("acclaro")]
+        Acclaro,
+        
+        [Description("translate_by_humans")]
+        TranslateByHumans,
+        
+        [Description("lingo24")]
+        Lingo24,
+        
+        [Description("assertio_language_services")]
+        AssertioLanguageServices,
+        
+        [Description("gte_localize")]
+        GteLocalize,
+        
+        [Description("kettu_solutions")]
+        KettuSolutions,
+        
+        [Description("languageline_solutions")]
+        LanguageLineTranslationSolutions
+        
+        // ReSharper restore IdentifierTypo
+    }
+}
