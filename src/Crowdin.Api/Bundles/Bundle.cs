1c56da71260c5695d53611da5be9b6ffd9ca761e
Max Inno <maksym.hubach@gmail.com>
2023-02-28 21:34:37 +0200

    API Updates: Bundles: new methods support, more tests, misc updates

--- src/Crowdin.Api/Bundles/Bundle.cs
+++ src/Crowdin.Api/Bundles/Bundle.cs
@@ -11,8 +11,11 @@ namespace Crowdin.Api.Bundles
         [JsonProperty("id")]
         public int Id { get; set; }
         
-        [JsonProperty("applicationId")]
-        public string ApplicationId { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        
+        [JsonProperty("format")]
+        public string Format { get; set; }
         
         [JsonProperty("sourcePatterns")]
         public string[] SourcePatterns { get; set; }
@@ -22,6 +25,9 @@ namespace Crowdin.Api.Bundles
         
         [JsonProperty("exportPattern")]
         public string ExportPattern { get; set; }
+        
+        [JsonProperty("isMultilingual")]
+        public bool IsMultilingual { get; set; }
         
         [JsonProperty("labelIds")]
         public int[] LabelIds { get; set; }
--- tests/Crowdin.Api.Tests/Bundles/BundlesApiTests.cs
+++ tests/Crowdin.Api.Tests/Bundles/BundlesApiTests.cs
@@ -1,7 +1,6 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -39,12 +38,12 @@ namespace Crowdin.Api.Tests.Bundles
                 });
             
             var executor = new BundlesApiExecutor(mockClient.Object);
-            ResponseList<Bundle> response = await executor.ListBundles(projectId);
+            ResponseList<Bundle>? response = await executor.ListBundles(projectId);
             
             Assert.NotNull(response);
             Assert.Single(response.Data);
-            Assert.Equal("/master/", response.Data[0].SourcePatterns[0]);
-            Assert.Equal(DateTimeOffset.Parse("2019-09-20T11:11:05+00:00"), response.Data[0].CreatedAt);
+
+            Assert_Bundle(response.Data[0]);
         }
         
         [Fact]
@@ -54,7 +53,8 @@ namespace Crowdin.Api.Tests.Bundles
             
             var request = new AddBundleRequest
             {
-                ApplicationId = "resx-string-exporter",
+                Name = "Resx bundle",
+                Format = "crowdin-resx",
                 SourcePatterns = new[]
                 {
                     "/master/"
@@ -64,6 +64,7 @@ namespace Crowdin.Api.Tests.Bundles
                     "/master/environments/"
                 },
                 ExportPattern = "strings-%two_letter_code%.resx",
+                IsMultilingual = false,
                 LabelIds = new[]
                 {
                     0
@@ -83,17 +84,55 @@ namespace Crowdin.Api.Tests.Bundles
                 .ReturnsAsync(new CrowdinApiResult
                 {
                     StatusCode = HttpStatusCode.OK,
-                    JsonObject = JObject.Parse(Core.Resources.Bundles.AddBundle_Response)
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.CommonResponses_Bundle)
                 });
             
             var executor = new BundlesApiExecutor(mockClient.Object);
-            Bundle response = await executor.AddBundle(projectId, request);
+            Bundle? response = await executor.AddBundle(projectId, request);
             
-            Assert.NotNull(response);
-            Assert.Equal(request.SourcePatterns.First(), response.SourcePatterns[0]);
-            Assert.Equal(DateTimeOffset.Parse("2019-09-20T11:11:05+00:00"), response.CreatedAt);
+            Assert_Bundle(response);
         }
-        
+
+        [Fact]
+        public async Task GetBundle()
+        {
+            const int projectId = 1, bundleId = 1;
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/bundles/{bundleId}";
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.CommonResponses_Bundle)
+                });
+
+            var executor = new BundlesApiExecutor(mockClient.Object);
+            Bundle? response = await executor.GetBundle(projectId, bundleId);
+            
+            Assert_Bundle(response);
+        }
+
+        [Fact]
+        public async Task DeleteBundle()
+        {
+            const int projectId = 1, bundleId = 2;
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/bundles/{bundleId}";
+
+            mockClient
+                .Setup(client => client.SendDeleteRequest(url, null))
+                .ReturnsAsync(HttpStatusCode.NoContent);
+
+            var executor = new BundlesApiExecutor(mockClient.Object);
+            await executor.DeleteBundle(projectId, bundleId);
+        }
+
         [Fact]
         public async Task EditBundle()
         {
@@ -105,8 +144,8 @@ namespace Crowdin.Api.Tests.Bundles
                 new BundlePatch
                 {
                     Operation = PatchOperation.Replace,
-                    Path = BundlePatchPath.ApplicationId,
-                    Value = "resx-string-exporter"
+                    Path = BundlePatchPath.Name,
+                    Value = "Resx bundle"
                 }
             };
             
@@ -123,15 +162,132 @@ namespace Crowdin.Api.Tests.Bundles
                 .ReturnsAsync(new CrowdinApiResult
                 {
                     StatusCode = HttpStatusCode.OK,
-                    JsonObject = JObject.Parse(Core.Resources.Bundles.EditBundle_Response)
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.CommonResponses_Bundle)
                 });
             
             var executor = new BundlesApiExecutor(mockClient.Object);
-            Bundle response = await executor.EditBundle(projectId, bundleId, patches);
+            Bundle? response = await executor.EditBundle(projectId, bundleId, patches);
+            
+            Assert_Bundle(response);
+        }
+
+        [Fact]
+        public async Task DownloadBundle()
+        {
+            const int projectId = 1, bundleId = 2;
+            const string exportId = "50fb3506-4127-4ba8-8296-f97dc7e3e0c3";
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/bundles/{bundleId}/exports/{exportId}/download";
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.DownloadBundle_Response)
+                });
+
+            var executor = new BundlesApiExecutor(mockClient.Object);
+            DownloadLink? response = await executor.DownloadBundle(projectId, bundleId, exportId);
             
             Assert.NotNull(response);
-            Assert.Equal("/master/environments/", response.IgnorePatterns[0]);
-            Assert.Equal(DateTimeOffset.Parse("2019-09-20T11:11:05+00:00"), response.CreatedAt);
+            Assert.NotEmpty(response.Url);
+            Assert.StartsWith("https", response.Url);
+            Assert.Equal(DateTimeOffset.Parse("2019-09-20T10:31:21+00:00"), response.ExpireIn);
+        }
+
+        [Fact]
+        public async Task ExportBundle()
+        {
+            const int projectId = 1, bundleId = 2;
+            
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/bundles/{bundleId}/exports";
+
+            mockClient
+                .Setup(client => client.SendPostRequest(url, null, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.Accepted,
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.CommonResponses_BundleExport)
+                });
+
+            var executor = new BundlesApiExecutor(mockClient.Object);
+            BundleExport? response = await executor.ExportBundle(projectId, bundleId);
+            
+            Assert_BundleExport(response);
+        }
+
+        [Fact]
+        public async Task CheckBundleExportStatus()
+        {
+            const int projectId = 1, bundleId = 2;
+            const string exportId = "50fb3506-4127-4ba8-8296-f97dc7e3e0c3";
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/bundles/{bundleId}/exports/{exportId}";
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Bundles.CommonResponses_BundleExport)
+                });
+            
+            var executor = new BundlesApiExecutor(mockClient.Object);
+            BundleExport? response = await executor.CheckBundleExportStatus(projectId, bundleId, exportId);
+            
+            Assert_BundleExport(response);
+        }
+
+        private static void Assert_Bundle(Bundle? model)
+        {
+            Assert.NotNull(model);
+            
+            Assert.Equal(1, model!.Id);
+            Assert.Equal("Resx bundle", model.Name);
+            Assert.Equal("crowdin-resx", model.Format);
+            
+            Assert.NotNull(model.SourcePatterns);
+            Assert.Single(model.SourcePatterns);
+            Assert.Equal("/master/", model.SourcePatterns[0]);
+            
+            Assert.NotNull(model.IgnorePatterns);
+            Assert.Single(model.IgnorePatterns);
+            Assert.Equal("/master/environments/", model.IgnorePatterns[0]);
+            
+            Assert.Equal("strings-%two_letters_code%.resx", model.ExportPattern);
+            Assert.False(model.IsMultilingual);
+            
+            Assert.NotNull(model.LabelIds);
+            Assert.Single(model.LabelIds);
+            Assert.Equal(0, model.LabelIds[0]);
+            
+            Assert.Equal(DateTimeOffset.Parse("2019-09-20T11:11:05+00:00"), model.CreatedAt);
+            Assert.Equal(DateTimeOffset.Parse("2019-09-20T12:22:20+00:00"), model.UpdatedAt);
+        }
+
+        private static void Assert_BundleExport(BundleExport? model)
+        {
+            Assert.NotNull(model);
+            
+            Assert.True(Guid.TryParse(model!.Identifier, out _));
+            Assert.Equal(BuildStatus.Finished, model.Status);
+            Assert.Equal(100, model.Progress);
+            
+            Assert.NotNull(model.Attributes);
+            Assert.Equal(2, model.Attributes.BundleId);
+            
+            DateTimeOffset date = DateTimeOffset.Parse("2019-09-23T11:26:54+00:00");
+            Assert.Equal(date, model.CreatedAt);
+            Assert.Equal(date, model.UpdatedAt);
+            Assert.Equal(date, model.StartedAt);
+            Assert.Equal(date, model.FinishedAt);
         }
     }
 }
