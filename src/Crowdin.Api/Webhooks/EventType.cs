8eb4fbf26c535f8553b7bc0ca499078f903cb622
Patryk Plewa <47867089+PatrykPlewaOfficial@users.noreply.github.com>
2023-05-04 15:18:05 +0300

    fix: Add missing webhook events (#121)
    Co-authored-by: Patryk Plewa <patryk.plewa>

--- src/Crowdin.Api/Webhooks/EventType.cs
+++ src/Crowdin.Api/Webhooks/EventType.cs
@@ -7,43 +7,79 @@ namespace Crowdin.Api.Webhooks
     [PublicAPI]
     public enum EventType
     {
+        [Description("file.added")]
+        FileAdded,
+
+        [Description("file.updated")]
+        FileUpdated,
+
+        [Description("file.reverted")]
+        FileReverted,
+
+        [Description("file.deleted")]
+        FileDeleted,
+
         [Description("file.translated")]
         FileTranslated,
-        
+
         [Description("file.approved")]
         FileApproved,
-        
+
         [Description("project.translated")]
         ProjectTranslated,
-        
+
         [Description("project.approved")]
         ProjectApproved,
-        
+
+        [Description("project.built")]
+        ProjectBuilt,
+
         [Description("translation.updated")]
         TranslationUpdated,
-        
+
         [Description("string.added")]
         StringAdded,
-        
+
         [Description("string.updated")]
         StringUpdated,
-        
+
         [Description("string.deleted")]
         StringDeleted,
 
+        [Description("stringComment.created")]
+        StringCommentCreated,
+
+        [Description("stringComment.updated")]
+        StringCommentUpdated,
+
+        [Description("stringComment.deleted")]
+        StringCommentDeleted,
+
+        [Description("stringComment.restored")]
+        StringCommentRestored,
+
         [Description("suggestion.added")]
         SuggestionAdded,
-        
+
         [Description("suggestion.updated")]
         SuggestionUpdated,
-        
+
         [Description("suggestion.deleted")]
         SuggestionDeleted,
-        
+
         [Description("suggestion.approved")]
         SuggestionApproved,
-        
+
         [Description("suggestion.disapproved")]
-        SuggestionDisapproved
+        SuggestionDisapproved,
+
+        [Description("task.added")]
+        TaskAdded,
+
+        [Description("task.statusChanged")]
+        TaskStatusChanged,
+
+        [Description("task.deleted")]
+        TaskDeleted,
     }
 }
