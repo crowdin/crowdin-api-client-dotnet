a83dbb0ac973891150416398909f8bcec0dd1f0b
Max Inno <maksym.hubach@gmail.com>
2023-06-12 20:45:41 +0300

    fix: Screenshots/Tags/AddTag: replace parse method & return type ParseResponseObject -> ParseResponseList

--- tests/Crowdin.Api.Tests/Screenshots/TagsApiTests.cs
+++ tests/Crowdin.Api.Tests/Screenshots/TagsApiTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 
@@ -9,6 +11,7 @@ using Crowdin.Api.Tests.Core.Resources;
 
 using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace Crowdin.Api.Tests.Screenshots
@@ -16,6 +19,63 @@ namespace Crowdin.Api.Tests.Screenshots
     public class TagsApiTests
     {
         private static readonly JsonSerializerSettings DefaultSettings = TestUtils.CreateJsonSerializerOptions();
+
+        [Fact]
+        public async Task AddTag()
+        {
+            const int projectId = 1;
+            const int screenshotId = 2;
+            
+            var requests = new[]
+            {
+                new AddTagRequest
+                {
+                    StringId = 328525,
+                    Position = new Position
+                    {
+                        X = 10,
+                        Y = 20,
+                        Height = 10,
+                        Width = 10
+                    }
+                }
+            };
+
+            string actualRequestJson = JsonConvert.SerializeObject(requests, DefaultSettings);
+            string expectedRequestJson = TestUtils.CompactJson(Screenshots_Tags.AddTag_Request);
+            Assert.Equal(expectedRequestJson, actualRequestJson);
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/screenshots/{screenshotId}/tags";
+
+            mockClient
+                .Setup(client => client.SendPostRequest(url, requests, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Screenshots_Tags.AddTag_Response)
+                });
+
+            var executor = new ScreenshotsApiExecutor(mockClient.Object);
+            ResponseList<Tag> response = await executor.AddTag(projectId, screenshotId, requests);
+            
+            Assert.NotNull(response);
+
+            Tag? tag = response.Data?.FirstOrDefault();
+            Assert.NotNull(tag);
+            Assert.Equal(98, tag!.Id);
+            Assert.Equal(2, tag.ScreenshotId);
+            Assert.Equal(2822, tag.StringId);
+            Assert.Equal(DateTimeOffset.Parse("2019-09-23T09:35:31+00:00"), tag.CreatedAt);
+
+            Position? position = tag.Position;
+            Assert.NotNull(position);
+            Assert.Equal(474, position.X);
+            Assert.Equal(147, position.Y);
+            Assert.Equal(99, position.Height);
+            Assert.Equal(490, position.Width);
+        }
         
         [Fact]
         public async Task ReplaceTags()
