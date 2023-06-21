0379076a8243d4e9e192bd3c9fbd03ccf9d1555d
Max Inno <maksym.hubach@gmail.com>
2023-06-21 22:00:38 +0300

    feat(api): add common column types + integration

---
+++ src/Crowdin.Api/ColumnType.cs
@@ -0,0 +1,25 @@
+﻿
+using JetBrains.Annotations;
+
+namespace Crowdin.Api
+{
+    [PublicAPI]
+    public static class ColumnType
+    {
+        public const string None = "none";
+        
+        public const string Identifier = "identifier";
+
+        public const string SourcePhrase = "sourcePhrase";
+
+        public const string SourceOrTranslation = "sourceOrTranslation";
+
+        public const string Translation = "translation";
+
+        public const string Context = "context";
+
+        public const string MaxLength = "maxLength";
+
+        public const string Labels = "labels";
+    }
+}
