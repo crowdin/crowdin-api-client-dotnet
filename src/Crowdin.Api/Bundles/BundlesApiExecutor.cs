12eff39ff6229f261023cded08378ae9a0ce356b
zahid92 <ansarizahid2098@gmail.com>
2024-02-03 18:33:06 +0530

    added ListBundleBranches

--- src/Crowdin.Api/Bundles/BundlesApiExecutor.cs
+++ src/Crowdin.Api/Bundles/BundlesApiExecutor.cs
@@ -1,11 +1,10 @@
-﻿
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
-using JetBrains.Annotations;
 
 using Crowdin.Api.Core;
 using Crowdin.Api.SourceFiles;
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Crowdin.Api.Bundles
 {
@@ -13,35 +12,51 @@ namespace Crowdin.Api.Bundles
     {
         private readonly ICrowdinApiClient _apiClient;
         private readonly IJsonParser _jsonParser;
-        
+
         public BundlesApiExecutor(ICrowdinApiClient apiClient)
         {
             _apiClient = apiClient;
             _jsonParser = apiClient.DefaultJsonParser;
         }
-        
+
         public BundlesApiExecutor(ICrowdinApiClient apiClient, IJsonParser jsonParser)
         {
             _apiClient = apiClient;
             _jsonParser = jsonParser;
         }
-        
+
         /// <summary>
         /// List bundles. Documentation:
-        /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.getMany">Crowdin API</a>
-        /// <a href="https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.bundles.getMany">Crowdin Enterprise API</a>
+        /// <a href="https://developer.crowdin.com/api/v2/string-based/#operation/api.projects.bundles.branches.getMany">Crowdin API</a>
+        /// <a href="https://developer.crowdin.com/enterprise/api/v2/string-based/#operation/api.projects.bundles.branches.getMany">Crowdin Enterprise API</a>
         /// </summary>
         [PublicAPI]
         public async Task<ResponseList<Bundle>> ListBundles(int projectId, int limit = 25, int offset = 0)
         {
             string url = FormUrl_Bundles(projectId);
-            
+
             IDictionary<string, string> queryParams = Utils.CreateQueryParamsFromPaging(limit, offset);
-            
+
             CrowdinApiResult result = await _apiClient.SendGetRequest(url, queryParams);
             return _jsonParser.ParseResponseList<Bundle>(result.JsonObject);
         }
-        
+
+        /// <summary>
+        /// List bundle branches. Documentation:
+        /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.getMany">Crowdin API</a>
+        /// <a href="https://developer.crowdin.com/enterprise/api/v2/#operation/api.projects.bundles.getMany">Crowdin Enterprise API</a>
+        /// </summary>
+        [PublicAPI]
+        public async Task<ResponseList<Branch>> ListBundleBranches(int projectId, int bundleId, int limit = 25, int offset = 0)
+        {
+            string url = FormUrl_BundleId(projectId, bundleId) + "/branches";
+
+            IDictionary<string, string> queryParams = Utils.CreateQueryParamsFromPaging(limit, offset);
+
+            CrowdinApiResult result = await _apiClient.SendGetRequest(url, queryParams);
+            return _jsonParser.ParseResponseList<Branch>(result.JsonObject);
+        }
+
         /// <summary>
         /// Add bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.post">Crowdin API</a>
@@ -54,7 +69,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendPostRequest(url, request);
             return _jsonParser.ParseResponseObject<Bundle>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Get bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.get">Crowdin API</a>
@@ -67,7 +82,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendGetRequest(url);
             return _jsonParser.ParseResponseObject<Bundle>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Delete bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.delete">Crowdin API</a>
@@ -80,7 +95,7 @@ namespace Crowdin.Api.Bundles
             HttpStatusCode statusCode = await _apiClient.SendDeleteRequest(url);
             Utils.ThrowIfStatusNot204(statusCode, $"Bundle {bundleId} removal failed");
         }
-        
+
         /// <summary>
         /// Edit bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.patch">Crowdin API</a>
@@ -93,7 +108,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendPatchRequest(url, patches);
             return _jsonParser.ParseResponseObject<Bundle>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Download Bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.exports.download.get">Crowdin API</a>
@@ -106,7 +121,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendGetRequest(url);
             return _jsonParser.ParseResponseObject<DownloadLink>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Export Bundle. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.exports.post">Crowdin API</a>
@@ -119,7 +134,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendPostRequest(url);
             return _jsonParser.ParseResponseObject<BundleExport>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Check Bundle Export Status. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.exports.get">Crowdin API</a>
@@ -132,7 +147,7 @@ namespace Crowdin.Api.Bundles
             CrowdinApiResult result = await _apiClient.SendGetRequest(url);
             return _jsonParser.ParseResponseObject<BundleExport>(result.JsonObject);
         }
-        
+
         /// <summary>
         /// Bundle list files. Documentation:
         /// <a href="https://developer.crowdin.com/api/v2/#operation/api.projects.bundles.files.getMany">Crowdin API</a>
@@ -145,23 +160,23 @@ namespace Crowdin.Api.Bundles
         {
             string url = FormUrl_BundleFiles(projectId, bundleId);
             IDictionary<string, string> queryParams = Utils.CreateQueryParamsFromPaging(limit, offset);
-            
+
             CrowdinApiResult result = await _apiClient.SendGetRequest(url, queryParams);
             return _jsonParser.ParseResponseList<T>(result.JsonObject);
         }
-        
+
         #region Helper methods
-        
+
         private static string FormUrl_Bundles(int projectId)
         {
             return $"/projects/{projectId}/bundles";
         }
-        
+
         private static string FormUrl_BundleId(int projectId, int bundleId)
         {
             return $"/projects/{projectId}/bundles/{bundleId}";
         }
-        
+
         private static string FormUrl_BundleFiles(int projectId, int bundleId)
         {
             return $"/projects/{projectId}/bundles/{bundleId}/files";
@@ -176,7 +191,8 @@ namespace Crowdin.Api.Bundles
         {
             return $"/projects/{projectId}/bundles/{bundleId}/exports/{exportId}";
         }
-        
+
+
         #endregion
     }
 }
