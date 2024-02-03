09e156761a0d92dfe27e8cff8c1fa0be4de3a5d3
zahid92 <ansarizahid2098@gmail.com>
2024-02-03 18:21:21 +0530

    added Clients api

---
+++ src/Crowdin.Api/Clients/ClientsApiExecutor.cs
@@ -0,0 +1,40 @@
+using Crowdin.Api.Core;
+using Crowdin.Api.Vendors;
+using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crowdin.Api.Clients
+{
+    internal class ClientsApiExecutor
+    {
+        private readonly ICrowdinApiClient _apiClient;
+        private readonly IJsonParser _jsonParser;
+
+        public ClientsApiExecutor(ICrowdinApiClient apiClient)
+        {
+            _apiClient = apiClient;
+            _jsonParser = apiClient.DefaultJsonParser;
+        }
+
+        public ClientsApiExecutor(ICrowdinApiClient apiClient, IJsonParser jsonParser)
+        {
+            _apiClient = apiClient;
+            _jsonParser = jsonParser;
+        }
+
+        /// <summary>
+        /// List Clients. Documentation:
+        /// <a href="https://developer.crowdin.com/enterprise/api/v2/#operation/api.clients.getMany">Crowdin Enterprise API</a>
+        /// </summary>
+        [PublicAPI]
+        public async Task<ResponseList<Client>> ListClients(int limit = 25, int offset = 0)
+        {
+            IDictionary<string, string> queryParams = Utils.CreateQueryParamsFromPaging(limit, offset);
+            CrowdinApiResult result = await _apiClient.SendGetRequest("/clients", queryParams);
+            return _jsonParser.ParseResponseList<Client>(result.JsonObject);
+        }
+    }
+}
---
+++ tests/Crowdin.Api.Tests/Clients/ClientsApiTests.cs
@@ -0,0 +1,45 @@
+using Crowdin.Api.Clients;
+using Crowdin.Api.Core;
+using Crowdin.Api.Tests.Core;
+using Moq;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Crowdin.Api.Tests.Clients
+{
+    public class ClientsApiTests
+    {
+        [Fact]
+        public async Task ListClients()
+        {
+            const int clientId = 1;
+            const string description = "John Smith Organization";
+
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/clients";
+            IDictionary<string, string> queryParams = TestUtils.CreateQueryParamsFromPaging();
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, queryParams))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Clients.ListClientsResponse)
+                });
+
+            var executor = new ClientsApiExecutor(mockClient.Object);
+            ResponseList<Client>? response = await executor.ListClients();
+
+            Assert.NotNull(response);
+            Assert.Single(response.Data);
+            Assert.IsType<Client>(response.Data[0]);
+            Assert.Equal(response.Data[0].Id, clientId);
+            Assert.Equal(response.Data[0].Description, description);
+        }
+    }
+}
