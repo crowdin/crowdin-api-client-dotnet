c686d88b12a4dda7f609f91693c68127711a11c7
Riju-bak <68413390+Riju-bak@users.noreply.github.com>
2024-03-18 03:58:20 -0400

    feat(api): Fields API Support (#229)

---
+++ src/Crowdin.Api/Fields/AddFieldRequest.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace Crowdin.Api.Fields
+{
+    [PublicAPI]
+    public class AddFieldRequest
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("slug")]
+        public string Slug { get; set; }
+
+        [JsonProperty("type")]
+        public FieldType Type { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("entities")]
+        public FieldEntity[] Entities { get; set; }
+
+        [JsonProperty("config")]
+        public FieldConfig Config { get; set; }
+
+    }
+}
