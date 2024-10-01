214b6034b56c6876ca45a678354fafb47e18a515
Max Inno <maksym.hubach@gmail.com>
2024-10-01 05:55:13 +0300

    feat(reports): add PreTranslateEfficiency support

---
+++ src/Crowdin.Api/Reports/PreTranslateEfficiencyGenerateReportRequest.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Collections.Generic;
+
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+#nullable enable
+
+namespace Crowdin.Api.Reports
+{
+    [PublicAPI]
+    public class PreTranslateEfficiencyGenerateReportRequest : GenerateReportRequest
+    {
+        [JsonProperty("name")]
+        public string Name => "pre-translate-efficiency";
+
+        [JsonProperty("schema")]
+        public SchemaBase Schema { get; set; } = null!;
+
+        [PublicAPI]
+        public abstract class SchemaBase
+        {
+            [JsonProperty("unit")]
+            public ReportUnit? Unit { get; set; }
+            
+            [JsonProperty("format")]
+            public ReportFormat? Format { get; set; }
+            
+            [JsonProperty("postEditingCategories")]
+            public ICollection<string>? PostEditingCategories { get; set; }
+        }
+
+        [PublicAPI]
+        public class GeneralSchema : SchemaBase
+        {
+            [JsonProperty("languageId")]
+            public string? LanguageId { get; set; }
+            
+            [JsonProperty("dateFrom")]
+            public DateTimeOffset? DateFrom { get; set; }
+            
+            [JsonProperty("dateTo")]
+            public DateTimeOffset? DateTo { get; set; }
+        }
+
+        [PublicAPI]
+        public class ByTaskSchema : SchemaBase
+        {
+            [JsonProperty("taskId")]
+            public int? TaskId { get; set; }
+        }
+    }
+}
