36988cb081595d71b0fa42a140a1c578160f9d44
Max Inno <40697586+innomaxx@users.noreply.github.com>
2024-11-01 09:34:16 +0200

    feat(translations): add List and Edit PreTranslation APIs (#277)

--- tests/Crowdin.Api.Tests/Translations/TranslationsApiTests.cs
+++ tests/Crowdin.Api.Tests/Translations/TranslationsApiTests.cs
@@ -10,6 +10,7 @@ using Crowdin.Api.Tests.Core;
 using Crowdin.Api.Translations;
 
 using Moq;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 
@@ -17,6 +18,70 @@ namespace Crowdin.Api.Tests.Translations
 {
     public class TranslationsApiTests
     {
+        private static readonly JsonSerializerSettings JsonSettings = TestUtils.CreateJsonSerializerOptions();
+        
+        [Fact]
+        public async Task ListPreTranslations()
+        {
+            const int projectId = 1;
+            
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            var url = $"/projects/{projectId}/pre-translations";
+            IDictionary<string, string> queryParams = TestUtils.CreateQueryParamsFromPaging();
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, queryParams))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Translations.CommonResources_PreTranslations_List)
+                });
+            
+            var executor = new TranslationsApiExecutor(mockClient.Object);
+            ResponseList<PreTranslation> response = await executor.ListPreTranslations(projectId);
+            
+            Assert_PreTranslation(response?.Data.FirstOrDefault());
+        }
+
+        [Fact]
+        public async Task EditPreTranslation()
+        {
+            const int projectId = 1;
+            const string preTranslationId = "pre-translation-id";
+
+            var patches = new[]
+            {
+                new PreTranslationPatch
+                {
+                    Operation = PatchOperation.Replace,
+                    Path = PreTranslationPatchPath.Status,
+                    Value = "status"
+                }
+            };
+            
+            string actualRequestJson = JsonConvert.SerializeObject(patches, JsonSettings);
+            string expectedRequestJson = TestUtils.CompactJson(Core.Resources.Translations.EditPreTranslation_Request);
+            Assert.Equal(expectedRequestJson, actualRequestJson);
+            
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+            
+            var url = $"/projects/{projectId}/pre-translations/{preTranslationId}";
+
+            mockClient
+                .Setup(client => client.SendPatchRequest(url, patches, null))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Core.Resources.Translations.CommonResources_PreTranslation)
+                });
+            
+            var executor = new TranslationsApiExecutor(mockClient.Object);
+            PreTranslation response = await executor.EditPreTranslation(projectId, preTranslationId, patches);
+            
+            Assert_PreTranslation(response);
+        }
+        
         [Fact]
         public async Task ApplyPreTranslationRequest()
         {
@@ -164,6 +229,32 @@ namespace Crowdin.Api.Tests.Translations
             Assert.Contains(2, attributes.LabelIds);
             Assert.Contains(3, attributes.LabelIds);
             Assert.Equal(4, attributes.ExcludeLabelIds.Single());
+        }
+
+        private static void Assert_PreTranslation(PreTranslation? preTranslation)
+        {
+            ArgumentNullException.ThrowIfNull(preTranslation);
+            
+            Assert.Equal("9e7de270-4f83-41cb-b606-2f90631f26e2", preTranslation.Identifier);
+            Assert.Equal(BuildStatus.Created, preTranslation.Status);
+            Assert.Equal(90, preTranslation.Progress);
+            
+            PreTranslateAttributes? attributes = preTranslation.Attributes;
+            ArgumentNullException.ThrowIfNull(attributes);
+            Assert.Equal(new[] { "uk" }, attributes.LanguageIds);
+            Assert.Equal(new[] { 742 }, attributes.FileIds);
+            Assert.Equal(PreTranslationMethod.Tm, attributes.Method);
+            Assert.Equal(AutoApproveOption.All, attributes.AutoApproveOption);
+            Assert.True(attributes.DuplicateTranslations);
+            Assert.True(attributes.SkipApprovedTranslations);
+            Assert.True(attributes.TranslateUntranslatedOnly);
+            Assert.True(attributes.TranslateWithPerfectMatchOnly);
+            
+            DateTimeOffset date = DateTimeOffset.Parse("2019-09-20T14:05:50+00:00");
+            Assert.Equal(date, preTranslation.CreatedAt);
+            Assert.Equal(date, preTranslation.UpdatedAt);
+            Assert.Equal(date, preTranslation.StartedAt);
+            Assert.Equal(date, preTranslation.FinishedAt);
         }
     }
 }
