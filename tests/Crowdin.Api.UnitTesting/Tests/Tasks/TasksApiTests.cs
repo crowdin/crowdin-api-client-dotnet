850315f63293450415c73ef1c0b267f1d6b59b34
EvilKot <flaringdragon@gmail.com>
2025-01-17 11:14:53 +0200

    feat(tasks): Allow querying tasks by multiple statuses (#286)

--- tests/Crowdin.Api.UnitTesting/Tests/Tasks/TasksApiTests.cs
+++ tests/Crowdin.Api.UnitTesting/Tests/Tasks/TasksApiTests.cs
@@ -1,5 +1,11 @@
-﻿using Crowdin.Api.Tasks;
+using Crowdin.Api.Core;
+using Crowdin.Api.Screenshots;
+using Crowdin.Api.Tasks;
+using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Net;
 using Xunit;
 
 namespace Crowdin.Api.UnitTesting.Tests.Tasks
@@ -75,6 +81,52 @@ namespace Crowdin.Api.UnitTesting.Tests.Tasks
             string actualRequestJson = JsonConvert.SerializeObject(request, Settings);
             string expectedRequestJson = TestUtils.CompactJson(Resources.Tasks.AddTask_RightRequestJson_VendorManual);
             Assert.Equal(expectedRequestJson, actualRequestJson);
+        }
+
+        [Fact]
+        public async System.Threading.Tasks.Task ListTasks()
+        {
+            const int projectId = 1;
+
+            var url = $"/projects/{projectId}/tasks";
+
+            var queryParams = new Dictionary<string, string>
+            {
+                { "limit", "25" },
+                { "offset", "0" },
+                { "status", "todo,in_progress" },
+                { "assigneeId", "1" },
+                { "orderBy", "createdAt desc,name asc" },
+
+            };
+
+            Mock<ICrowdinApiClient> mockClient = TestUtils.CreateMockClientWithDefaultParser();
+
+            mockClient
+                .Setup(client => client.SendGetRequest(url, queryParams))
+                .ReturnsAsync(new CrowdinApiResult
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    JsonObject = JObject.Parse(Resources.Tasks.ListTasks)
+                });
+
+            var executor = new TasksApiExecutor(mockClient.Object);
+            var statuses = new TaskStatus[] { TaskStatus.Todo, TaskStatus.InProgress };
+            var assigneeId = 1;
+            var sortingRules = new SortingRule[] {
+                new SortingRule() { Field = "createdAt", Order = SortingOrder.Descending },
+                new SortingRule() { Field = "name", Order = SortingOrder.Ascending }
+            };
+
+            var response = await executor.ListTasks(projectId, 25, 0, statuses, assigneeId, sortingRules);
+
+            Assert.NotNull(response);
+
+            Assert.Equal(25, response.Pagination?.Limit);
+            Assert.Equal(0, response.Pagination?.Offset);
+
+            Assert.Single(response.Data);
+            Assert.Equal(2, response.Data[0].Id);
         }
     }
 }
