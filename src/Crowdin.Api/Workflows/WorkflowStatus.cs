91aabca3b542b52b78b896f7f2181c222a0307d9
Max Inno <maksym.hubach@gmail.com>
2025-03-03 00:02:45 +0200

    feat(workflows): add ListStringsOnTheWorkflowStep method support

---
+++ src/Crowdin.Api/Workflows/WorkflowStatus.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using JetBrains.Annotations;
+
+namespace Crowdin.Api.Workflows
+{
+    [PublicAPI]
+    public enum WorkflowStatus
+    {
+        [Description("todo")]
+        ToDo,
+        
+        [Description("done")]
+        Done,
+        
+        [Description("pending")]
+        Pending,
+        
+        [Description("incomplete")]
+        Incomplete,
+        
+        [Description("need_review")]
+        NeedReview
+    }
+}
