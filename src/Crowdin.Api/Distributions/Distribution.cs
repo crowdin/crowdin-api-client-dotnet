45ebd20439127e317c924a0b3dec4ab17f3d1ffc
Max Inno <40697586+innomaxx@users.noreply.github.com>
2025-03-03 09:10:20 +0000

    feat(distributions): add support for "manifestUrl" (#307)

--- src/Crowdin.Api/Distributions/Distribution.cs
+++ src/Crowdin.Api/Distributions/Distribution.cs
@@ -3,35 +3,28 @@ using System;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 
-#nullable enable
-
 namespace Crowdin.Api.Distributions
 {
     [PublicAPI]
     public class Distribution
     {
         [JsonProperty("hash")]
-#pragma warning disable CS8618
         public string Hash { get; set; }
-#pragma warning restore CS8618
+        
+        [JsonProperty("manifestUrl")]
+        public string ManifestUrl { get; set; }
         
         [JsonProperty("exportMode")]
         public DistributionExportMode ExportMode { get; set; }
-        
+
         [JsonProperty("name")]
-#pragma warning disable CS8618
         public string Name { get; set; }
-#pragma warning restore CS8618
-        
+
         [JsonProperty("fileIds")]
-#pragma warning disable CS8618
         public int[] FileIds { get; set; }
-#pragma warning restore CS8618
-        
+
         [JsonProperty("bundleIds")]
-#pragma warning disable CS8618
         public int[] BundleIds { get; set; }
-#pragma warning restore CS8618
         
         [JsonProperty("createdAt")]
         public DateTimeOffset CreatedAt { get; set; }
