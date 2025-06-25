572ac3bce86e83ae8ef6087e072780600fe1d5e7
Max Inno <maksym.hubach@gmail.com>
2025-06-25 13:11:00 +0300

    fix(string-comments): migrate Int32 -> Int64 for ID

--- src/Crowdin.Api/StringComments/StringComment.cs
+++ src/Crowdin.Api/StringComments/StringComment.cs
@@ -9,7 +9,7 @@ namespace Crowdin.Api.StringComments
     public class StringComment
     {
         [JsonProperty("id")]
-        public int Id { get; set; }
+        public long Id { get; set; }
         
         [JsonProperty("text")]
         public string Text { get; set; }
